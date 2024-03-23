16b8a84daeb0a55021c687bedc9a048bf60db5b4
briheet01 <briheetyadav@gmail.com>
2024-03-23 11:59:37 +0530

    sql

--- config.go
+++ config.go
@@ -19,7 +19,7 @@ var Envs = initConfig()
 func initConfig() Config {
 	return Config{
 		Port:       getEnv("PORT", "8080"),
-		DBUser:     getEnv("DB_USER", "root"),
+		DBUser:     getEnv("DB_USER", "briheet"),
 		DBPassword: getEnv("DB_PASSWORD", "password"),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
 		DBName:     getEnv("DB_NAME", "projectmanager"),
