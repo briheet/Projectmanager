9951d1210591b432fd8cedede148a45fa7a60acf
briheet01 <briheetyadav@gmail.com>
2024-03-24 15:37:46 +0530

    tests

--- tasks_test.go
+++ tasks_test.go
@@ -35,5 +35,38 @@ func TestCreateTask(t *testing.T) {
 		router.HandleFunc("/tasks", service.handleCreateTasks)
 
 		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Error("invalid status code, it should fail")
+		}
+	})
+
+	t.Run("should create a task", func(t *testing.T) {
+		payload := &Task{
+			Name:         "Create a project",
+			ProjectID:    1,
+			AssignedToID: 42,
+		}
+
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/tasks", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/tasks", service.handleCreateTasks)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
 	})
 }
